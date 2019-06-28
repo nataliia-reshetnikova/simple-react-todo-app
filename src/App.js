28cdff86bf62599df4def8cc447c92167f18f658
nataliereshetnikova <ab1748@gmail.com>
2019-06-28 10:24:34 -0400

    correct bug in package.json

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends Component {
                 SAVE
                 </button>
               </form>
-              <ToDoList todos={this.state.todos} />
+              <ol><ToDoList todos={this.state.todos} /></ol>
         </div>
     );
   }
